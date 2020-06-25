fa08b276f57d1e45ccc47984ede890dea775a69e
RyoMikami-v2 <r.mikami.sys19@morijyobi.ac.jp>
2020-06-25 11:52:23 +0900

    結果にコミット

--- delete.php
+++ delete.php
@@ -1,21 +1,24 @@
 <?php
-$dsn = 'mysql:dbname=sample_db;host=localhost;';
-$user = 'hatake';
-$password = 'Morijyobi';
-try {
-    $dbh = new PDO($dsn, $user, $password);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    $id = $_GET['id'];
+    $dsn = 'mysql:dbname=sample_db;host=localhost;';
+    $user = 'mikami';
+    $password = 'mysinsql';
+    try {
+        $dbh = new PDO($dsn, $user, $password);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = "delete from user where id = :id";
-    $stmt = $dbh->prepare($sql);
-    $params = array(':id' => $id);
-    $stmt -> execute($params);
-    header('Location: index.php?fg = 1');
-} catch (PDOException $e) {
-    //echo "接続失敗: " . $e->getMessage() . "\n";
-    header('Location: index.php?fg = 1? err=$e -> getMessage()');
-    exit();
-}
+        $id = $_GET['id'];
+
+
+        $sql = "delete from user where id = :id"; 
+        $stmt = $dbh->prepare($sql);
+        $params = array(':id' => $id);
+        $stmt->execute($params);
+
+        
+        header('Location: index.php?fg=1');
+        
+    } catch (PDOException $e) {
+        header('Location: index.php?fg=2?err='. $e->getMessage());
+        exit();
+    }
 ?>
